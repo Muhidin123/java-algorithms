8fa01d54ebfc403c11cdbc2382a8c9579920d091
Muhidin <muhidinhukic1@gmail.com>
2021-03-25 12:33:02 -0500

    bagOfTokens Leet code

--- src/main/java/Algorithms.java
+++ src/main/java/Algorithms.java
@@ -1,6 +1,40 @@
+import java.util.Arrays;
 
 public class Algorithms {
+        public static void main(String[] args) {
+        }
 
+        public static int bagOfTokensScore(int[] tokens, int P) {
+            Arrays.sort(tokens);
+
+            if (tokens.length == 0 || P < tokens[0]) return 0;
+            int left = 0, right = tokens.length - 1, points = 0;
+
+            while (left <= right) {
+                if (P >= tokens[left]) {
+
+                    points++;
+
+                    P -= tokens[left];
+
+                    left++;
+
+                } else {
+                    if (right - left > 1) {
+
+                        P += tokens[right];
+
+                        right--;
+
+                        points--;
+                    } else break;
+                }
+            }
+            return points;
+        }
+
+
+        
 
 
 }
